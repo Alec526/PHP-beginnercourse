fc1b8a8c3ae451175215da894a03bc6ad3280e5e
Alec526 <aarnaoutoglou@roc-dev.com>
2021-04-21 20:36:26 +0200

    4 opdrachten van h3 af

---
+++ H03/Opdracht2/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$array = ["img_0500.jpg", "lillypilly1.jpg", "Maranchery_Biyyam_Kayal_kandal.jpg", "weeping-elm.jpg", "weeping-elm0091"];
+
+foreach($array as $boom){
+    echo "<img src='../img2/".$boom."'>";
+}
