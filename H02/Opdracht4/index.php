54b430c8d2c44516b619351942742377b73ce95e
Alec526 <aarnaoutoglou@roc-dev.com>
2021-04-21 16:59:48 +0200

    H1 af

---
+++ H02/Opdracht4/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = "\"omgaan";
+$b = "met";
+$c = "teleu";
+
+$volledigeZin = "De cursus " . $a . " met " . $c . "rstelling" . "\"" . " kan helaas vanavond niet doorgaan.";
+
+echo $volledigeZin;
