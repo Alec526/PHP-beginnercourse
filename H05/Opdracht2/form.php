c57d946fc683023d53dd3578e42c4872aadc5ecf
Alec526 <aarnaoutoglou@roc-dev.com>
2021-06-03 10:52:27 +0200

    idek

--- H05/Opdracht2/form.php
+++ H05/Opdracht2/form.php
@@ -2,18 +2,18 @@
 
 if($_POST['voornaam'] == ""){
     echo "Je moet nog een naam invullen";
-    echo "<br><a href='formulier.html'>Terug naar het formulier</a>";
+    echo "<br><a href='formulier.html'>Terug naar het formulier</a><br>";
 };
 if($_POST['adres'] == ""){
     echo "Je moet nog een adres invullen";
-    echo "<br><a href='formulier.html'>Terug naar het formulier</a>";
+    echo "<br><a href='formulier.html'>Terug naar het formulier</a><br>";
 };
 if($_POST['email'] == ""){
     echo "Je moet nog een email invullen";
-    echo "<br><a href='formulier.html'>Terug naar het formulier</a>";
+    echo "<br><a href='formulier.html'>Terug naar het formulier</a><br>";
 };
 if($_POST['wachtwoord'] == ""){
     echo "Je moet nog een wachtwoord invullen";
-    echo "<br><a href='formulier.html'>Terug naar het formulier</a>";
+    echo "<br><a href='formulier.html'>Terug naar het formulier</a><br>";
 };
 
