efc0a94af29a26afcc73e39da414e064f397797a
Alec526 <aarnaoutoglou@roc-dev.com>
2021-06-22 14:48:46 +0200

    h6 remake cause i did a bad job

--- H06/Opdracht1/database.php
+++ H06/Opdracht1/database.php
@@ -1,9 +1,32 @@
 <?php
 
-$Username = "root";
-$Password = "";
+$servername = "localhost";
+$dbname = "phpschool";
+$username = "root";
+$password = "";
 
 try {
+    $dbconn = new PDO("mysql:host={$servername};dbname={$dbname}", $username, $password);
+    $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"."<br>";
+} catch (PDOException $exc) {
+    echo "Connection failed: " . $exc->getMessage();
+};
+
+$query = "select * from cursist";
+
+$stmt = $dbconn->prepare($query) or die ("Error 1.");
+$stmt->execute() or die ("Error 2.");
+$row = $stmt->fetch() or die ("Error 3.");
+
+while ($row = $stmt->fetch()) {
+
+}
+
+
+
+
+/*try {
     $dbh = new PDO('mysql:host=localhost;dbname=school;port=3306', $Username, $Password);
     foreach ($dbh->query('SELECT * from cursist') as $row) {
         JSC($row);
@@ -15,9 +38,11 @@ try {
     die();
 }
 
+
 function JSC($input){
 
     echo "<pre>";
     print_r($input);
     echo "</pre>";
 }
+*/
---
+++ H06/Opdracht1/db.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+        table, tr, td {
+            border: solid 1px black;
+            border-collapse: collapse;
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+<table>
+    <?php
+
+    $servername = "localhost";
+    $dbname = "phpschool";
+    $username = "root";
+    $password = "";
+
+    try {
+        $dbconn = new PDO("mysql:host={$servername};dbname={$dbname}", $username, $password);
+        $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo "Connected successfully"."<br>";
+    } catch (PDOException $exc) {
+        echo "Connection failed: " . $exc->getMessage();
+    };
+
+    $query = "select * from cursist";
+
+    $stmt = $dbconn->prepare($query) or die ("Error 1.");
+    $stmt->execute() or die ("Error 2.");
+    $row = $stmt->fetch() or die ("Error 3.");
+
+    while ($row = $stmt->fetch()) {
+        echo "<tr>";
+        echo "<td>" . $row['cursistnr'] . "</td>";
+        echo "<td>" . $row['naam'] . "</td>";
+        echo "<td>" . $row['roepnaam'] . "</td>";
+        echo "<td>" . $row['straat'] . "</td>";
+        echo "<td>" . $row['postcode'] . "</td>";
+        echo "<td>" . $row['plaats'] . "</td>";
+        echo "<td>" . $row['geslacht'] . "</td>";
+        echo "<td>" . $row['geb_datum'] . "</td>";
+        echo "<tr>";
+    }
+
+    ?>
+</table>
+</body>
+</html>
