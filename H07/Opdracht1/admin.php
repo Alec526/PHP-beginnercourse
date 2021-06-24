79bb5cf66dd9e209a90c51bb476d3b560c00c7fc
Alec526 <aarnaoutoglou@roc-dev.com>
2021-06-24 11:58:06 +0200

    h7 af

--- H07/Opdracht1/admin.php
+++ H07/Opdracht1/admin.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-if (isset ($_SESSION['gebruiker']) && $_SESSION['gebruiker'] ['rol'] == "admin") {
-    echo "welkom admin, " . $_SESSION['gebruiker'] ['inlognaam'];
+if (isset ($_SESSION['gebruikers']) && $_SESSION['gebruikers'] ['rol'] == "admin") {
+    echo "welkom admin, " . $_SESSION['gebruikers'] ['inlognaam'];
     echo "<br>" . '<a href="websession.php?logout">log uit</a>';
 } else {
     header('location: websession.php?logout');
