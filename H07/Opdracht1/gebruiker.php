a3ee7ba17a81c89d6f50efc0cf911eb9075adbca
Alec526 <aarnaoutoglou@roc-dev.com>
2021-09-05 15:06:39 +0200

    schema

--- H07/Opdracht1/gebruiker.php
+++ H07/Opdracht1/gebruiker.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-if (isset ($_SESSION['gebruiker'])) {
-    echo "welkom, " . $_SESSION['gebruiker'] ['inlognaam'];
+if (isset ($_SESSION['gebruikers'])) {
+    echo "welkom, " . $_SESSION['gebruikers'] ['inlognaam'];
     echo "<br>" . "<a href='websession.php?logout'>log uit</a>";
 } else {
     header('location: websession.php?logout');
