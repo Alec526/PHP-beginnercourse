96453ae3077d2a986617db817a034b3669f0cd04
Alec526 <aarnaoutoglou@roc-dev.com>
2021-10-15 11:30:24 +0200

    s

--- H07/Opdracht1/gebruiker.php
+++ H07/Opdracht1/gebruiker.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 if (isset ($_SESSION['gebruikers'])) {
     echo "welkom, " . $_SESSION['gebruikers'] ['inlognaam'];
     echo "<br>" . "<a href='websession.php?logout'>log uit</a>";
